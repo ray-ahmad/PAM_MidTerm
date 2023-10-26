3aa0df0b9ce7788fe2b3f18708be8a54f60b2eed
ray-ahmad <ray.ahmdr@gmail.com>
2023-10-26 20:11:14 +0700

    Fix Logout Feature

--- app/src/main/java/com/rayahmad/pammidterm/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/rayahmad/pammidterm/ui/dashboard/DashboardFragment.kt
@@ -1,5 +1,6 @@
 package com.rayahmad.pammidterm.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,8 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
+import com.rayahmad.pammidterm.LoginActivity
+import com.rayahmad.pammidterm.MainActivity
 import com.rayahmad.pammidterm.databinding.FragmentDashboardBinding
 import com.rayahmad.pammidterm.repository.UserRepository
 import kotlinx.coroutines.launch
@@ -33,19 +36,24 @@ class DashboardFragment : Fragment() {
         _binding = FragmentDashboardBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        userRepository = UserRepository(requireContext())
+
 //        val textView: TextView = binding.textDashboard
 //        dashboardViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
-        return root
-//        val logoutButton = findViewById<Button>(R.id.logoutButton)
         binding.logoutButton.setOnClickListener {
             lifecycleScope.launch {
-                Toast.makeText(requireContext(), "Anda telah logout", Toast.LENGTH_SHORT).show()
                 userRepository.logout()
-                // Anda bisa menambahkan logika untuk berpindah ke halaman login atau halaman lain setelah logout
+
+                Toast.makeText(requireContext(), "Anda telah logout", Toast.LENGTH_SHORT).show()
+                val intent = Intent(requireActivity(), LoginActivity::class.java)
+                startActivity(intent)
+
+                requireActivity().finish()
             }
         }
+        return root
     }
 
     override fun onDestroyView() {
